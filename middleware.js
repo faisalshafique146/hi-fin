a7d6d3745d913721e622eebcc6be6e5bcf534c3f
Faisal Shafique <faisalshafique146@gmail.com>
2025-09-19 17:48:50 +0500

    refactor: remove unused arcjet middleware and streamline clerk middleware implementation

--- middleware.js
+++ middleware.js
@@ -1,4 +1,3 @@
-import arcjet, { createMiddleware, detectBot, shield } from '@arcjet/next';
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 const isProtectedRoute = createRouteMatcher([
@@ -7,26 +6,7 @@ const isProtectedRoute = createRouteMatcher([
     "/transaction(.*)"
 ])
 
-const aj = arcjet({
-  key: process.env.ARCJET_KEY,
-  // characteristics: ["userId"], // Track based on Clerk userId
-  rules: [
-    // Shield protection for content and security
-    shield({
-      mode: "LIVE",
-    }),
-    detectBot({
-      mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
-      allow: [
-        "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
-        "GO_HTTP", // For Inngest
-        // See the full list at https://arcjet.com/bot-list
-      ],
-    }),
-  ],
-});
-
-const clerk = clerkMiddleware(async (auth, req) => {
+export default clerkMiddleware(async (auth, req) => {
     const { userId } = await auth();
 
     if (!userId && isProtectedRoute(req)) {
@@ -34,8 +14,6 @@ const clerk = clerkMiddleware(async (auth, req) => {
         return redirectToSignIn()
     }
 });
-
-export default createMiddleware(aj, clerk)
 
 export const config = {
     matcher: [
